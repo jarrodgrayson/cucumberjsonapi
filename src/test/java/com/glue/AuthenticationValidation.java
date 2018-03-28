3ccb63e7ccbc500e5e9af67f19a5e562b8873e98
Jarrod Grayson <jarrodgrayson@qasymphony.com>
2018-03-28 21:26:22 +0000

    Test

--- src/test/java/com/glue/AuthenticationValidation.java
+++ src/test/java/com/glue/AuthenticationValidation.java
@@ -1,11 +1,10 @@
 package com.glue;
 
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
 import org.junit.Assert;
 
-public class Kid_Logging_outStep {
+public class Happy_Path_Login {
 
     @When("^I attempt to login with invalid credentials$")
     public void Iattempttologinwithinvalidcredentials() throws Throwable {
